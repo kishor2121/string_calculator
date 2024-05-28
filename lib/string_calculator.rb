efdf36f24a0cdae8cd6bbc4673053c78fc9b3dcb
kishorkumarh <96061723+kishorkumarh@users.noreply.github.com>
2024-05-28 18:03:40 +0530

    kishorkumar | Handle New Lines Between Numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,6 +3,6 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(",").map(&:to_i).reduce(0, :+)
+    numbers.split(/,|\n/).map(&:to_i).reduce(0, :+)
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,29 +4,10 @@ require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
   describe ".add" do
-    context "given an empty string" do
-      it "returns 0" do
-        expect(StringCalculator.add("")).to eq(0)
-      end
-    end
-
-    context "given a single number" do
-      it "returns the number" do
-        expect(StringCalculator.add("1")).to eq(1)
-      end
-    end
-
-    context "given two numbers" do
+    context "given numbers separated by new lines" do
       it "returns the sum of the numbers" do
-        expect(StringCalculator.add("1,5")).to eq(6)
-      end
-    end
-
-    context "given multiple numbers" do
-      it "returns the sum of the numbers" do
-        expect(StringCalculator.add("1,2,3")).to eq(6)
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
       end
     end
   end
 end
-
