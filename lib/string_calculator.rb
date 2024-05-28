aa9d0047cc0c58ad06748716d75818c1f574018a
kishorkumarh <96061723+kishorkumarh@users.noreply.github.com>
2024-05-28 18:06:01 +0530

    kishorkumar |  Support Different Delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,6 +3,12 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/,|\n/).map(&:to_i).reduce(0, :+)
+    delimiter = ",|\n"
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers[4..]
+    end
+
+    numbers.split(/#{delimiter}/).map(&:to_i).reduce(0, :+)
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,9 +4,9 @@ require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
   describe ".add" do
-    context "given numbers separated by new lines" do
+    context "given a custom delimiter" do
       it "returns the sum of the numbers" do
-        expect(StringCalculator.add("1\n2,3")).to eq(6)
+        expect(StringCalculator.add("//;\n1;2")).to eq(3)
       end
     end
   end
